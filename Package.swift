1c2f35376f4a19fa7c35a557e87d6bfe855caff5
manzick <manzurov.alex@yahoo.com>
2024-10-23 15:54:26 +0600

    WIP

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CarrotSDK",
-            url: "https://github.com/manzick/ManzickTest/releases/download/1.0.1/CarrotSDK.xcframework.zip",
+            url: "https://github.com/manzick/ManzickTest/releases/download/2.12.11/CarrotSDK.xcframework.zip",
             checksum: "9ca863f0d36f27bcecb534101a64f389f318b10d091fd8bcee291773a682d9fe"
         ),
     ]
